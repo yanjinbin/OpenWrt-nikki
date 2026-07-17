da9dee7706f9884236c3c7f4da1edc21723bd957
Yem JinBen <yanjinbin@gmail.com>
2026-07-17 17:29:48 +0800

    chore: localize LuCI patch installer

--- install-luci-patch.sh
+++ install-luci-patch.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# Install LuCI/RPC changes from this fork without rebuilding packages.
+# 从当前 fork 安装魔改后的 LuCI/RPC 文件，无需重新编译软件包。
 
 set -e
 
 RAW_BASE="${NIKKI_RAW_BASE:-https://github.com/yanjinbin/OpenWrt-nikki/raw/refs/heads/main}"
 GITHUB_PROXY="${GITHUB_PROXY:-https://gh-proxy.com/}"
+DESTDIR="${DESTDIR:-}"
+RESTART_SERVICES="${RESTART_SERVICES:-1}"
 
 raw_url() {
 	if [ -n "$GITHUB_PROXY" ]; then
@@ -19,12 +21,14 @@ download() {
 	local source_path="$1"
 	local target_path="$2"
 	local tmp_path="${target_path}.tmp"
+	local full_target_path="${DESTDIR}${target_path}"
+	local full_tmp_path="${DESTDIR}${tmp_path}"
 
-	echo "download $source_path"
-	mkdir -p "$(dirname "$target_path")"
-	wget -O "$tmp_path" "$(raw_url "$source_path")"
-	mv -f "$tmp_path" "$target_path"
-	chmod 0644 "$target_path"
+	echo "下载 $source_path"
+	mkdir -p "$(dirname "$full_target_path")"
+	wget -O "$full_tmp_path" "$(raw_url "$source_path")"
+	mv -f "$full_tmp_path" "$full_target_path"
+	chmod 0644 "$full_target_path"
 }
 
 download "luci-app-nikki/htdocs/luci-static/resources/tools/nikki.js" \
@@ -36,11 +40,15 @@ download "luci-app-nikki/htdocs/luci-static/resources/view/nikki/profile.js" \
 download "luci-app-nikki/root/usr/share/rpcd/ucode/luci.nikki" \
 	"/usr/share/rpcd/ucode/luci.nikki"
 
-echo "restart rpcd and uhttpd"
-/etc/init.d/rpcd restart
-/etc/init.d/uhttpd restart
+if [ "$RESTART_SERVICES" = "1" ]; then
+	echo "重启 rpcd 和 uhttpd"
+	/etc/init.d/rpcd restart
+	/etc/init.d/uhttpd restart
+else
+	echo "跳过服务重启"
+fi
 
-echo "clear luci cache"
-rm -rf /tmp/luci-indexcache /tmp/luci-modulecache
+echo "清理 LuCI 缓存"
+rm -rf "${DESTDIR}/tmp/luci-indexcache" "${DESTDIR}/tmp/luci-modulecache"
 
-echo "success"
+echo "安装完成"
