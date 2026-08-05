6b4e2639c8202daaf208bc6e40022d7bd310e5cf
Yem JinBen <yanjinbin@gmail.com>
2026-08-06 00:52:50 +0800

    feat(nikki): default mixin options to unmodified

--- nikki/files/uci-defaults/init.sh
+++ nikki/files/uci-defaults/init.sh
@@ -5,10 +5,6 @@
 # check nikki.config.init
 init=$(uci -q get nikki.config.init); [ -z "$init" ] && return
 
-# fixed api secret and authentication password
-uci set nikki.mixin.api_secret='666666'
-uci set nikki.@authentication[0].password='666666'
-
 # remove nikki.config.init
 uci del nikki.config.init
 
